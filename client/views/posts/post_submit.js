01662038cf94a60324f9c4494b2dc52a10c5d552
ken <ken@innomonster.com>
2014-09-01 20:46:44 +0800

    prepare #5

---
+++ client/views/posts/post_submit.js
@@ -0,0 +1,18 @@
+Template.postSubmit.events({
+    'submit form': function(e) {
+        e.preventDefault();
+
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val(),
+            message: $(e.target).find('[name=message]').val()
+        }
+
+        Meteor.call('post', post, function(error, id) {
+            if (error)
+                return alert(error.reason);
+
+            Router.go('postPage', {_id: id});
+        });
+    }
+});
