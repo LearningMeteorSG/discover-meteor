2ee0baca9862a5fbcc92539f7916e11ff3a9d97a
ken <ken@innomonster.com>
2014-10-08 00:13:48 +0800

    #7 comments

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,10 +1,10 @@
 Template.postItem.helpers({
-  ownPost: function() {
-    return this.userId == Meteor.userId();
-  },
-  domain: function() {
-    var a = document.createElement('a');
-    a.href = this.url;
-    return a.hostname;
-  }
+	ownPost: function() {
+		return this.userId == Meteor.userId();
+	},
+	domain: function() {
+		var a = document.createElement('a');
+		a.href = this.url;
+		return a.hostname;
+	}
 });
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,9 @@
 Meteor.publish('posts', function() {
    return Posts.find();
 });
+
+Meteor.publish('comments', function(postId) {
+	return Comments.find({
+		postId: postId
+	});
+});
